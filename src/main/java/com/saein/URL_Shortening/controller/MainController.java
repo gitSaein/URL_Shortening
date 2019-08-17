d1682b485b15aab8b330f03a438b33f022c88c9c
saein.lee <gitSaein@gmail.com>
2019-08-17 17:44:25 +0900

    controller mock test

--- src/main/java/com/saein/URL_Shortening/controller/MainController.java
+++ src/main/java/com/saein/URL_Shortening/controller/MainController.java
@@ -1,11 +1,11 @@
 package com.saein.URL_Shortening.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class MainController {
 	@Value("${spring.application.name}")
 	String appName;
