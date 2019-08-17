beb6c87beddb9a56d5ce4274d4d0e6b2dda99ec2
saein.lee <gitSaein@gmail.com>
2019-08-17 21:13:35 +0900

    URL Shortener 1차 화면 개발

--- src/main/java/com/saein/URL_Shortening/controller/MainRestController.java
+++ src/main/java/com/saein/URL_Shortening/controller/MainRestController.java
@@ -1,17 +1,18 @@
 package com.saein.URL_Shortening.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-public class MainController {
+@RestController
+public class MainRestController {
 	@Value("${spring.application.name}")
 	String appName;
 	
-	@GetMapping("/")
+	@GetMapping("/hello")
 	public String hello(Model model) {
 		return "hello";
 	}
+
 }
