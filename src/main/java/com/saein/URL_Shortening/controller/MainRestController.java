a6405129d83cb6fdb6cd2b73c38ebaa3bda8c7e4
saein.lee <gitSaein@gmail.com>
2019-08-18 17:35:03 +0900

    thymeleaf form 얀동중

--- src/main/java/com/saein/URL_Shortening/controller/MainRestController.java
+++ src/main/java/com/saein/URL_Shortening/controller/MainRestController.java
@@ -1,8 +1,14 @@
 package com.saein.URL_Shortening.controller;
 
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.saein.URL_Shortening.vo.Url;
+import com.saein.URL_Shortening.vo.Url2;
 
 @RestController
 public class MainRestController {
@@ -10,6 +16,12 @@ public class MainRestController {
 	@GetMapping("/hello")
 	public String hello(Model model) {
 		return "hello";
+	}
+	
+	@PostMapping("/create")
+	public Url2 createUrl(@ModelAttribute(value="url") Url2 url, BindingResult result, Model model) {
+		System.out.println("create");
+		return url;
 	}
 
 }
---
+++ src/main/java/com/saein/URL_Shortening/vo/Url2.java
@@ -0,0 +1,20 @@
+package com.saein.URL_Shortening.vo;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import lombok.Data;
+
+@Data
+public class Url2 {
+	private String origin;
+	
+}
